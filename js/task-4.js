a9a1f171c3ccf1eed016ee4d002e00a78f158824
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-06-09 14:45:22 +0300

    v.7.1.4 (+task-4)

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,29 @@
+const inputFormUser = document.querySelector(`.login-form`);
+const submit = document.querySelector(`.login-form`);
+    inputFormUser.addEventListener(`submit`, hendlerGetSubmit);
+    
+    function hendlerGetSubmit (event){
+    event.preventDefault();
+    
+    // console.log(`submit`);  // перевырка роботи submit
+    // console.dir(event.currentTarget);  // перевірка форми
+
+    const {email, password} = event.currentTarget.elements;  //  деструктурізація введених знавчень
+    const emailUser = (email.value).replace(" ", "");
+    const passwordUser = (password.value).replace(" ", "");
+    
+    if(emailUser === "" || passwordUser === "") {
+        alert (`All form fields must be filled in`)
+    } else {
+        const data = {
+            email: emailUser,
+            password: passwordUser,
+        }
+        console.dir(data);
+        }
+}
+
+
+
+
 
