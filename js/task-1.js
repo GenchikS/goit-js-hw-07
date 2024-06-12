ac46fefe687cd2256a3cd2870b23f7a724f440ca
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-06-12 21:42:29 +0300

    v.7.2.3

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,18 @@
 const categories = document.querySelectorAll(`.item`);
-const category = document.querySelectorAll(`.title`);
-const elements = document.querySelectorAll(`.list`);
-console.log(`Number of categories: ${categories.length}`)
+const categoriesAll = Array.from(categories);
+console.log(`Number of categories: ${categoriesAll.length}`)
+// categoriesAll.result ()
+
+// console.log(categoriesAll);
+// const category = document.querySelectorAll(`.item>h2`);
 
 
-const result = category.forEach(function(category, index, el){
-    console.dir(`Category: ${category.textContent}`);
-       console.log(`Elements:`, elements.length);
+
+const result =  compelArr.forEach(function(category, index, el){
+
+    // const elements = document.querySelectorAll(`.list`);
+    console.log(category)
+    // console.dir(`Category: ${category.textContent}`);
+    // console.log(`Elements:`, elements.length);
 })
 
--- js/task-3.js
+++ js/task-3.js
@@ -2,9 +2,12 @@ const inputUser = document.querySelector(`#name-input`);
 const output = document.querySelector(`#name-output`);
 
 inputUser.addEventListener("input", (event) => {
-    output.textContent = event.currentTarget.value;
-    if (output.textContent.endsWith(` `) || output.textContent.startsWith(` `)){
+        output.textContent = event.currentTarget.value;
+    if (!output.textContent.includes(` `) || !output.textContent.includes(``)){
+        output.textContent = (event.currentTarget.value).trim();
+    } else{
         output.textContent = "Anonymous";
+        
     }
   });
 
