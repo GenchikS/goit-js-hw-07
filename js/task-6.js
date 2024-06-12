67f2f374f2a6a2af4d8b02b6991deb43217d9726
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-06-12 10:05:27 +0300

    v.7.2.6 (+- finish)

--- js/task-6.js
+++ js/task-6.js
@@ -1,48 +1,3 @@
-// const boxes = document.querySelector(`#boxes`);
-// console.dir(boxes);
-// const inputUser = document.querySelector(`.input`);
-// const createkUser = document.querySelector(`button[data-create]`);
-// const destroyUser = document.querySelector(`button[data-destroy]`);
-
-
-// inputUser.addEventListener(`change`, (event)=> {
-//   event.preventDefault();
-  
-//   // console.log(event);  //  перевірка об'єкту
-//     // console.log(event.currentTarget.value);  //  перевірка вводу
-//     const divCreate = event.currentTarget.value;
-   
-//     createkUser.addEventListener(`click`, (event)=> {  //  добавлення <div>
-//       // console.log(event);  //  перевірка об'єкту
-//       // console.log("create");  //  перевірка  clickUser
-//     if (divCreate >= 1 && divCreate <= 100 ){
-//       for (let i = 1; i <= divCreate; i++ ){
-//         const heading = document.createElement(`div`);
-//         heading.classList.add("title-task-six");
-//         boxes.append(heading);
-        
-
-//         // const titleTaskSix = document.querySelector(`.title-task-six`)
-//         // titleTaskSix.style.width = `${ i * 10 }px`;
-//         // titleTaskSix.style.height =  `${ i * 10 }px`;
-//         // titleTaskSix.style.backgroundColor = `#${Math.floor(Math.random() * 16777215 )}`
-        
-//       };
-//       } else {alert(`Введіть число від 1 до 100!`)};
-//     });
-//     event.currentTarget.value = "";
-        
-//     destroyUser.addEventListener(`click`, (event)=> {   //  видалення <div>
-//       // console.log("delete");  //  перевірка  clickUser
-//       // console.log(divCreate)  // перевірка введеного числа
-
-//     for (let i = divCreate; i != 0; i-- ){
-//         const heading = document.querySelector(`.title-task-six`);
-//         heading.remove();
-//       };
-// });
-// })
-
 const inputUser = document.querySelector(`.input`);
 const createClickUser = document.querySelector(`button[data-create]`)
 const destroyClickUser = document.querySelector(`button[data-destroy]`)
@@ -50,15 +5,15 @@ const destroyClickUser = document.querySelector(`button[data-destroy]`)
 
 inputUser.addEventListener(`change`, (event) => {
   event.preventDefault();
-  const inputUser = event.currentTarget.value; 
-  event.currentTarget.value = "";
-      // console.log((inputUser));  //  перевірка вводу
+  let inputUser = event.currentTarget.value;
+      event.currentTarget.value = "";  //  видалення введеного числа
+     // console.log((inputUser));  //  перевірка вводу
       
       let widthUser = 10;
       let heighthUser = 10;
 
   createClickUser.addEventListener("click", createClick);
-destroyClickUser.addEventListener("click", destroyClick);
+  destroyClickUser.addEventListener("click", destroyClick);
 
 
   function createClick(){
@@ -84,12 +39,11 @@ destroyClickUser.addEventListener("click", destroyClick);
                   .toString(16)
                   .padStart(6, 0)}`;
               }
-
-            };
-          } else {alert(`Введіть число від 1 до 100!`)};
-          // document.querySelector(`.input`).reset();
-         
+             };
+            } else {alert(`Введіть число від 1 до 100!`)};
+          inputUser = "0";
     };
+ 
         
 
     function destroyClick() {   //  ф-ція видалення вмісту контейнера boxes
