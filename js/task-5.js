992a0a600d66dad8b22e28cb82d8627da3ed6a35
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-06-12 22:15:04 +0300

    v.7.2.5 (+)

--- js/task-5.js
+++ js/task-5.js
@@ -8,8 +8,10 @@ btClick.addEventListener(`click`, getRandomHexColor);
 
 function getRandomHexColor() {   //  використання ф-ції для генерації кольору
     color.textContent = `#${Math.floor(Math.random() * 16777215 )}`;
-    changeColor.style.backgroundColor = `#${Math.floor(Math.random() * 16777215 )}`;  //  переклад для виводу на rgb
-    
+    changeColor.style.backgroundColor = `#${Math.floor(Math.random() * 16777215)
+          .toString(16)
+          .padStart(6, 0)}`;  //  переклад для виводу на rgb
+
   return `#${Math.floor(Math.random() * 16777215)
           .toString(16)
           .padStart(6, 0)}`;
