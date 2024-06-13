a3143a315d40f0ea7dba2da930991df954f36f9f
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-06-13 23:43:59 +0300

    v.7.3.3(+3)

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,22 @@
 const inputUser = document.querySelector(`#name-input`);
 const output = document.querySelector(`#name-output`);
 
-inputUser.addEventListener("input", (event) => {
-        output.textContent = event.currentTarget.value;
-    if (output.textContent.includes(` `) || !output.textContent.includes(``)){
-        output.textContent = (event.currentTarget.value).trim();
-    } else{
-        output.textContent = "Anonymous";
-    }
-});
+
+inputUser.addEventListener("keydown", event => {
+        // output.textContent = event.key;
+        // console.log("Key: ", event.code);
+        // console.log("Key: ", event.key);
+        // console.log("Key: ", event);
+        
+        if (event.code === "Space"|| output.textContent === ""){
+            output.textContent = `Anonymous`;
+        } else {
+            inputUser.addEventListener("input", event => {
+            output.textContent = event.currentTarget.value.trim();
+        });
+        }
+        
+    });
 
 
 
